0067e8f7b015df38bbc87754cd568b3accfe77c9
qiu-deqing <deqing.qiu@gmail.com>
2015-05-23 15:31:10 +0800

    add structure and style

---
+++ page-scroll.js
@@ -0,0 +1,16 @@
+var pageScroll = (function (w) {
+  var doc = w.document;
+
+  var pageScroll = {
+    init: function () {
+
+    },
+    getScrollPos: function (win) {
+      win = win || w;
+
+
+    }
+  };
+
+  return pageScroll;
+}(window));
